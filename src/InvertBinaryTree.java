30b27a59940256096af35a9497b7af34e8a57f01
Crainax <413997411@qq.com>
2016-10-21 22:44:03 +0800

    add two solution

---
+++ src/InvertBinaryTree.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by crainax on 2016/10/21.
+ */
+public class InvertBinaryTree {
+}
---
+++ src/InvertBinaryTree_2.java
@@ -0,0 +1,24 @@
+/**
+ * Created by crainax on 2016/10/21.
+ */
+public class InverBinaryTree {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+
+        if (root ==null)
+            return null;
+
+        
+
+    }
+}
